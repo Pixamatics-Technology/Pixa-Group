b9881342e8b239ef315f809c903f278950fe860f
unknown <info@pixamatics.com>
2021-03-31 16:47:51 +0600

    changes added

--- js/custom.js
+++ js/custom.js
@@ -1,60 +1,60 @@
 (function ($) {
-  "use strict";
+	"use strict";
 
-  var review = $('.review_part_text');
-  if (review.length) {
-    review.owlCarousel({
-      items: 2,
-      loop: true,
-      dots: true,
-      autoplay: true,
-      margin: 40,
-      autoplayHoverPause: true,
-      autoplayTimeout:5000,
-      nav: false,
-      responsive: {
-        0:{
-          items: 1
-        },
-        480:{
-          items: 1
-        },
-        769:{
-          items: 2
-        }
-    }
-    });
-  }
-  $('.popup-youtube, .popup-vimeo').magnificPopup({
-    // disableOn: 700,
-    type: 'iframe',
-    mainClass: 'mfp-fade',
-    removalDelay: 160,
-    preloader: false,
-    fixedContentPos: false
-});
-  // menu fixed js code
-  $(window).scroll(function () {
-    var window_top = $(window).scrollTop() + 1;
-    if (window_top > 50) {
-      $('.main_menu').addClass('menu_fixed animated fadeInDown');
-    } else {
-      $('.main_menu').removeClass('menu_fixed animated fadeInDown');
-    }
-  });
-  if (document.getElementById('default-select')) {
+	var review = $('.review_part_text');
+	if (review.length) {
+		review.owlCarousel({
+			items: 2,
+			loop: true,
+			dots: true,
+			autoplay: true,
+			margin: 40,
+			autoplayHoverPause: true,
+			autoplayTimeout: 5000,
+			nav: false,
+			responsive: {
+				0: {
+					items: 1
+				},
+				480: {
+					items: 1
+				},
+				769: {
+					items: 2
+				}
+			}
+		});
+	}
+	$('.popup-youtube, .popup-vimeo').magnificPopup({
+		// disableOn: 700,
+		type: 'iframe',
+		mainClass: 'mfp-fade',
+		removalDelay: 160,
+		preloader: false,
+		fixedContentPos: false
+	});
+	// menu fixed js code
+	$(window).scroll(function () {
+		var window_top = $(window).scrollTop() + 1;
+		if (window_top > 50) {
+			$('.main_menu').addClass('menu_fixed animated fadeInDown');
+		} else {
+			$('.main_menu').removeClass('menu_fixed animated fadeInDown');
+		}
+	});
+	if (document.getElementById('default-select')) {
 		$('select').niceSelect();
 	}
 
-  // page-scroll
-  $('.page-scroll').bind('click', function(event) {
-    var $anchor = $(this);
-    var headerH = '80';
-    $('html, body').stop().animate({
-        scrollTop: $($anchor.attr('href')).offset().top - headerH + "px"
-    }, 1500, 'easeInOutExpo');
-    event.preventDefault();
- });
+	// page-scroll
+	$('.page-scroll').bind('click', function (event) {
+		var $anchor = $(this);
+		var headerH = '80';
+		$('html, body').stop().animate({
+			scrollTop: $($anchor.attr('href')).offset().top - headerH + "px"
+		}, 1500, 'easeInOutExpo');
+		event.preventDefault();
+	});
 
 }(jQuery));
 
@@ -67,68 +67,68 @@
 (function ($) {
 	$.fn.countTo = function (options) {
 		options = options || {};
-		
+
 		return $(this).each(function () {
 			// set options for current element
 			var settings = $.extend({}, $.fn.countTo.defaults, {
-				from:            $(this).data('from'),
-				to:              $(this).data('to'),
-				speed:           $(this).data('speed'),
+				from: $(this).data('from'),
+				to: $(this).data('to'),
+				speed: $(this).data('speed'),
 				refreshInterval: $(this).data('refresh-interval'),
-				decimals:        $(this).data('decimals')
+				decimals: $(this).data('decimals')
 			}, options);
-			
+
 			// how many times to update the value, and how much to increment the value on each update
 			var loops = Math.ceil(settings.speed / settings.refreshInterval),
 				increment = (settings.to - settings.from) / loops;
-			
+
 			// references & variables that will change with each update
 			var self = this,
 				$self = $(this),
 				loopCount = 0,
 				value = settings.from,
 				data = $self.data('countTo') || {};
-			
+
 			$self.data('countTo', data);
-			
+
 			// if an existing interval can be found, clear it first
 			if (data.interval) {
 				clearInterval(data.interval);
 			}
 			data.interval = setInterval(updateTimer, settings.refreshInterval);
-			
+
 			// initialize the element with the starting value
 			render(value);
-			
+
 			function updateTimer() {
 				value += increment;
 				loopCount++;
-				
+
 				render(value);
-				
-				if (typeof(settings.onUpdate) == 'function') {
+
+				if (typeof (settings.onUpdate) == 'function') {
 					settings.onUpdate.call(self, value);
 				}
-				
+
 				if (loopCount >= loops) {
 					// remove the interval
 					$self.removeData('countTo');
 					clearInterval(data.interval);
 					value = settings.to;
-					
-					if (typeof(settings.onComplete) == 'function') {
+
+					if (typeof (settings.onComplete) == 'function') {
 						settings.onComplete.call(self, value);
 					}
 				}
 			}
-			
+
 			function render(value) {
 				var formattedValue = settings.formatter.call(self, value, settings);
 				$self.html(formattedValue);
 			}
 		});
 	};
-	
+
 	$.fn.countTo.defaults = {
 		from: 0,               // the number the element should start at
 		to: 0,                 // the number the element should end at
@@ -139,26 +139,52 @@
 		onUpdate: null,        // callback method for every time the element is updated
 		onComplete: null       // callback method for when the element finishes updating
 	};
-	
+
 	function formatter(value, settings) {
 		return value.toFixed(settings.decimals);
 	}
 }(jQuery));
 
 jQuery(function ($) {
-  // custom formatting example
-  $('.count-number').data('countToOptions', {
-	formatter: function (value, options) {
-	  return value.toFixed(options.decimals).replace(/\B(?=(?:\d{3})+(?!\d))/g, ',');
+	// custom formatting example
+	$('.count-number').data('countToOptions', {
+		formatter: function (value, options) {
+			return value.toFixed(options.decimals).replace(/\B(?=(?:\d{3})+(?!\d))/g, ',');
+		}
+	});
+
+	// start all the timers
+	$('.timer').each(count);
+
+	function count(options) {
+		var $this = $(this);
+		options = $.extend({}, options || {}, $this.data('countToOptions') || {});
+		$this.countTo(options);
 	}
-  });
-  
-  // start all the timers
-  $('.timer').each(count);  
-  
-  function count(options) {
-	var $this = $(this);
-	options = $.extend({}, options || {}, $this.data('countToOptions') || {});
-	$this.countTo(options);
-  }
+});
+
+
+
+/* partner slider */
+$(document).ready(function () {
+	$('.customer-logos').slick({
+		slidesToShow: 6,
+		slidesToScroll: 1,
+		autoplay: true,
+		autoplaySpeed: 1500,
+		arrows: false,
+		dots: false,
+		pauseOnHover: false,
+		responsive: [{
+			breakpoint: 768,
+			settings: {
+				slidesToShow: 4
+			}
+		}, {
+			breakpoint: 520,
+			settings: {
+				slidesToShow: 3
+			}
+		}]
+	});
 });
